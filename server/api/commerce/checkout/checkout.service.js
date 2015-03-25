67c4c90922b76ab211c03f04f0b921912b7ac431
Jesse Cogollo <cogollo87@gmail.com>
2015-03-25 17:57:42 -0500

    more changes.....

--- server/api/commerce/checkout/checkout.service.js
+++ server/api/commerce/checkout/checkout.service.js
@@ -3,25 +3,31 @@
 var loanService = require('../../loan/loan.service')
 var config = require('../../../config/environment');
 var tdCommerceService = require('TDCore').commerceService;
-
+var tdUserService = require('TDCore').userService;
 
 exports.placeOrder = function(user, cartId, addresses, orderData, cb){
   tdCommerceService.init(config.connections.commerce);
   tdCommerceService.customerCreate(user, function(err, customer) {
     if(err) {return cb(err);}
-    tdCommerceService.cartAddress(cartId, addresses, function(err, data) {
+    user.mageCustomerId = customer;
+    tdUserService.init(config.connections.user);
+    tdUserService.save(user, function(err, userPrepared){
       if(err) {return cb(err);}
-      tdCommerceService.cartUpdateCustomer(cartId.cartId, customer, function(err, data) {
+      tdCommerceService.init(config.connections.commerce);
+      tdCommerceService.cartAddress(cartId, addresses, function(err, data) {
         if(err) {return cb(err);}
-        tdCommerceService.cartSetShipping(cartId,function(err, dataShipping) {
+        tdCommerceService.cartUpdateCustomer(cartId.cartId, userPrepared, function(err, data) {
           if(err) {return cb(err);}
-          tdCommerceService.cartSetPayment(cartId, orderData.payment, function(err, dataPayment) {
+          tdCommerceService.cartSetShipping({cartId:cartId.cartId,shippingMethod:config.commerce.shippingMethod},function(err, dataShipping) {
             if(err) {return cb(err);}
-            tdCommerceService.cartPlace(cartId, function(err, dataOrderId) {
+            tdCommerceService.cartSetPayment(cartId.cartId, orderData.payment, function(err, dataPayment) {
               if(err) {return cb(err);}
-              tdCommerceService.addCommentToOrder(dataOrderId, JSON.stringify(orderData), 'pending', function(err, comment) {
+              tdCommerceService.cartPlace(cartId.cartId, function(err, dataOrderId) {
                 if(err) {return cb(err);}
-                return cb(null, dataOrderId);
+                tdCommerceService.addCommentToOrder(dataOrderId, JSON.stringify(orderData), 'pending', function(err, comment) {
+                  if(err) {return cb(err);}
+                  return cb(null, dataOrderId);
+                });
               });
             });
           });
