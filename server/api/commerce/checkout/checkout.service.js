5148f28289efb1f72c8241b08adff210465db9dc
Jesse Cogollo <cogollo87@gmail.com>
2015-03-26 10:06:53 -0500

    finish placeOrder

--- server/api/commerce/checkout/checkout.service.js
+++ server/api/commerce/checkout/checkout.service.js
@@ -24,7 +24,7 @@ exports.placeOrder = function(user, cartId, addresses, orderData, cb){
               if(err) {return cb(err);}
               tdCommerceService.cartPlace(cartId.cartId, function(err, dataOrderId) {
                 if(err) {return cb(err);}
-                tdCommerceService.addCommentToOrder(dataOrderId, JSON.stringify(orderData), 'pending', function(err, comment) {
+                tdCommerceService.orderCommentAdd(dataOrderId, JSON.stringify(orderData), 'pending', function(err, comment) {
                   if(err) {return cb(err);}
                   return cb(null, dataOrderId);
                 });
