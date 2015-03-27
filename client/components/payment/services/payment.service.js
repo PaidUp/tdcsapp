b24c61366c581042f87fc0c3fbf363891423f794
Zuly <zulyalcaraz@gmail.com>
2015-03-27 17:41:50 -0500

    refactor payment bar

--- client/components/payment/services/payment.service.js
+++ client/components/payment/services/payment.service.js
@@ -9,7 +9,6 @@ angular.module('convenienceApp')
     var CardPayment = $resource('/api/v1/payment/card/:action', {}, {});
 
     this.sendPayment = function (payment) {
-      console.log('payment', payment);
       return Payment.save(payment).$promise;
     };
 
