9f38ae879e6e4eec466c9d1301c078a2e25e29d4
Zuly <zulyalcaraz@gmail.com>
2015-04-07 11:42:54 -0500

    Resolve bug in profile when load and update contact and addresses to first time

--- client/app/user/account/account.controller.js
+++ client/app/user/account/account.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('convenienceApp')
-  .controller('AccountCtrl', function ($scope, $rootScope, UserService, AuthService, FlashService, ModalService) {
+  .controller('AccountCtrl', function ($scope, $rootScope, $state, UserService, AuthService, FlashService, ModalService) {
 
     $rootScope.$emit('bar-welcome', {
       left:{
@@ -15,60 +15,62 @@ angular.module('convenienceApp')
     $scope.user = angular.extend({}, AuthService.getCurrentUser());
     $scope.oldUser = angular.extend({}, AuthService.getCurrentUser());
     $scope.oldShippingPhone = {};
+    $scope.oldShippingAddress = {};
+    $scope.oldBillingAddress = {};
     $scope.shipping = {
       address: {}
     };
     $scope.billing = {
       address: {}
     };
-    $scope.oldShippingAddress = {};
-    $scope.oldBillingAddress = {};
     $scope.modal = ModalService;
     $scope.states = UserService.getStates();
 
-    UserService.getContactList($scope.user._id).then(function(data){
-      angular.forEach(data, function (contactInfo) {
-        UserService.getContact($scope.user._id, contactInfo.contactId).then(function (contact){
-          if (contact.label === 'shipping') {
-            $scope.oldShippingPhone = contact;
-            $scope.shipping.phone = contact.value;
-          }
-        }).catch(function (err) {
-          $scope.sendAlertErrorMsg(err.data.message);
-        });
-      });
-    }).catch(function (err) {
-      $scope.sendAlertErrorMsg(err.data.message);
-    });
-
-    UserService.listAddresses($scope.user._id).then(function (data) {
-      angular.forEach(data, function (address) {
-        UserService.getAddress($scope.user._id, address.addressId).then(function (addressObj) {
-          if (addressObj.type === 'shipping') {
-            $scope.oldShippingAddress = addressObj;
-            $scope.shipping.address = angular.extend({}, addressObj);
-          } else if (addressObj.type === 'billing') {
-            $scope.oldBillingAddress = addressObj;
-            $scope.billing.address = angular.extend({}, addressObj);
-          }
-          if (Object.keys($scope.oldShippingAddress).length !== 0 &&
-              Object.keys($scope.oldBillingAddress).length !== 0) {
-            if ($scope.oldShippingAddress.address1 === $scope.oldBillingAddress.address1 &&
-                $scope.oldShippingAddress.address2 === $scope.oldBillingAddress.address2 &&
-                $scope.oldShippingAddress.city === $scope.oldBillingAddress.city &&
-                $scope.oldShippingAddress.state === $scope.oldBillingAddress.state &&
-                $scope.oldShippingAddress.zipCode === $scope.oldBillingAddress.zipCode) {
-              $scope.sameBillingAsShiping = true;
+    AuthService.isLoggedInAsync(function (loggedIn) {
+      UserService.getContactList($scope.user._id).then(function (data){
+        angular.forEach(data, function (contactInfo) {
+          UserService.getContact($scope.user._id, contactInfo.contactId).then(function (contact){
+            if (contact.label === 'shipping') {
+              $scope.oldShippingPhone = contact;
+              $scope.shipping.phone = contact.value;
             }
-          }
-        }).catch(function (err) {
-          $scope.sendAlertErrorMsg(err.data.message);
+          }).catch(function (err) {
+            $scope.sendAlertErrorMsg(err.data.message);
+          });
         });
+      }).catch(function (err) {
+        $scope.sendAlertErrorMsg(err.data.message);
       });
-    }).catch(function (err) {
-      $scope.sendAlertErrorMsg(err.data.message);
-    });
 
+      UserService.listAddresses($scope.user._id).then(function (data) {
+        angular.forEach(data, function (address) {
+          UserService.getAddress($scope.user._id, address.addressId).then(function (addressObj) {
+            if (addressObj.type === 'shipping') {
+              $scope.oldShippingAddress = addressObj;
+              $scope.shipping.address   = angular.extend({}, addressObj);
+            } else if (addressObj.type === 'billing') {
+              $scope.oldBillingAddress = addressObj;
+              $scope.billing.address   = angular.extend({}, addressObj);
+            }
+            if (Object.keys($scope.oldShippingAddress).length !== 0 &&
+                Object.keys($scope.oldBillingAddress).length !== 0) {
+              if ($scope.oldShippingAddress.address1 === $scope.oldBillingAddress.address1 &&
+                  $scope.oldShippingAddress.address2 === $scope.oldBillingAddress.address2 &&
+                  $scope.oldShippingAddress.city     === $scope.oldBillingAddress.city     &&
+                  $scope.oldShippingAddress.state    === $scope.oldBillingAddress.state    &&
+                  $scope.oldShippingAddress.zipCode  === $scope.oldBillingAddress.zipCode) {
+                $scope.sameBillingAsShiping = true;
+              }
+            }
+          }).catch(function (err) {
+            $scope.sendAlertErrorMsg(err.data.message);
+          });
+        });
+      }).catch(function (err) {
+        $scope.sendAlertErrorMsg(err.data.message);
+      });
+    });
+    
     $scope.sendAlertUpdateProfileSuccess = function () {
       FlashService.addAlert({
         type: 'success',
@@ -88,14 +90,14 @@ angular.module('convenienceApp')
     $scope.updateProfile = function () {
       UserService.updateUser({
         firstName: $scope.user.firstName,
-        lastName: $scope.user.lastName,
-        userId: $scope.user._id
-      }).then(function (){
+        lastName:  $scope.user.lastName,
+        userId:    $scope.user._id
+      }).then(function () {
         $scope.sendAlertUpdateProfileSuccess();
         angular.extend(AuthService.getCurrentUser(), $scope.user);
         $scope.oldUser = angular.extend({}, AuthService.getCurrentUser());
-      }).catch(function () {
-
+      }).catch(function (err) {
+        $scope.sendAlertErrorMsg(err.data.message);
       });
     };
 
@@ -110,18 +112,18 @@ angular.module('convenienceApp')
     };
 
     $scope.updatePassword = function () {
-      AuthService.updatePassword($scope.user.password, $scope.user.newPassword, $scope.user._id).then(function (){
-          delete $scope.user.password;
-          delete $scope.user.newPassword;
-          delete $scope.user.confirmNewPassword;
-          FlashService.addAlert({
-            type: 'success',
-            templateUrl: 'components/application/directives/alert/alerts/update-password-success.html',
-            timeout: 5000
-          });
-        }).catch(function (err){
-          $scope.sendAlertErrorMsg(err.data.message);
+      AuthService.updatePassword($scope.user.password, $scope.user.newPassword, $scope.user._id).then(function () {
+        delete $scope.user.password;
+        delete $scope.user.newPassword;
+        delete $scope.user.confirmNewPassword;
+        FlashService.addAlert({
+          type: 'success',
+          templateUrl: 'components/application/directives/alert/alerts/update-password-success.html',
+          timeout: 5000
         });
+      }).catch(function (err){
+        $scope.sendAlertErrorMsg(err.data.message);
+      });
     };
 
     $scope.updateAddress = function (address) {
@@ -140,22 +142,23 @@ angular.module('convenienceApp')
       }).catch(function (err) {
         $scope.sendAlertErrorMsg(err.data.message);
       });
+      $state.reload();
     };
 
     $scope.createInfoPhone = function (phone, label) {
       var phoneInfo = {
-        label: label,
-        type: 'telephone',
-        value: phone,
+        label:  label,
+        type:  'telephone',
+        value:  phone,
         userId: $scope.user._id
       };
-
       UserService.createContact(phoneInfo).then(function (data) {
         phoneInfo.contactId = data.contactId;
         $scope.sendAlertUpdateProfileSuccess();
       }).catch(function (err) {
         $scope.sendAlertErrorMsg(err.data.message);
       });
+      $state.reload();
     };
 
     $scope.updateInfoPhone = function (phoneInfo) {
@@ -171,8 +174,8 @@ angular.module('convenienceApp')
       $scope.submitted = true;
 
       if ($scope.profileForm.$dirty && $scope.profileForm.$valid) {
-        if ($scope.user.firstName !== $scope.oldUser.firstName ||
-              $scope.user.lastName !== $scope.oldUser.lastName) {
+        if ($scope.user.firstName !== $scope.oldUser.firstName ||    
+            $scope.user.lastName  !== $scope.oldUser.lastName) {
           // submit profile update
           $scope.updateProfile();
           $scope.profileForm.$setPristine();
@@ -196,14 +199,14 @@ angular.module('convenienceApp')
         // Object.keys(a) if for check empty object
         if (Object.keys($scope.oldShippingPhone).length !== 0) {
           // submit shipping phone update
-          $scope.oldShippingPhone.value = $scope.shipping.phone;
+          $scope.oldShippingPhone.value  = $scope.shipping.phone;
           $scope.oldShippingPhone.userId = $scope.user._id;
           $scope.updateInfoPhone($scope.oldShippingPhone);
           $scope.shippingPhoneForm.$setPristine();
         } else {
           // submit create shipping phone
           $scope.createInfoPhone($scope.shipping.phone, 'shipping');
-          $scope.oldShippingPhone.value = $scope.shipping.phone;
+          $scope.oldShippingPhone.value  = $scope.shipping.phone;
           $scope.oldShippingPhone.userId = $scope.user._id;
           $scope.shippingPhoneForm.$setPristine();
         }
@@ -211,35 +214,40 @@ angular.module('convenienceApp')
 
       if ($scope.shippingForm.$dirty &&
           $scope.shipping.address.address1 && $scope.shipping.address.address1 !== '' &&
-          $scope.shipping.address.city && $scope.shipping.address.city !== '' &&
+          $scope.shipping.address.city     && $scope.shipping.address.city     !== '' &&
           $scope.shipping.address.address2 && $scope.shipping.address.address2 !== '' &&
-          $scope.shipping.address.zipCode && $scope.shipping.address.zipCode !== '' &&
-          $scope.shipping.address.state && $scope.shipping.address.state !== '') {
+          $scope.shipping.address.zipCode  && $scope.shipping.address.zipCode  !== '' &&
+          $scope.shipping.address.state    && $scope.shipping.address.state    !== '') {
 
         if (Object.keys($scope.oldShippingAddress).length !== 0) {
           // update shipping address
           $scope.shipping.address.userId = $scope.user._id;
           $scope.oldShippingAddress = angular.extend({}, $scope.shipping.address);
           $scope.updateAddress($scope.oldShippingAddress);
+
           if ($scope.sameBillingAsShiping) {
             if (Object.keys($scope.oldBillingAddress).length !== 0) {
+
               $scope.oldBillingAddress.address1 = $scope.shipping.address.address1;
               $scope.oldBillingAddress.address2 = $scope.shipping.address.address2;
-              $scope.oldBillingAddress.city = $scope.shipping.address.city;
-              $scope.oldBillingAddress.state = $scope.shipping.address.state;
-              $scope.oldBillingAddress.zipCode = $scope.shipping.address.zipCode;
-              $scope.oldBillingAddress.userId = $scope.user._id;
-              $scope.oldBillingAddress.label = 'billing';
-              $scope.oldBillingAddress.type = 'billing';
-              $scope.oldBillingAddress.country = 'USA';
+              $scope.oldBillingAddress.city     = $scope.shipping.address.city;
+              $scope.oldBillingAddress.state    = $scope.shipping.address.state;
+              $scope.oldBillingAddress.zipCode  = $scope.shipping.address.zipCode;
+              $scope.oldBillingAddress.userId   = $scope.user._id;
+              $scope.oldBillingAddress.label    = 'billing';
+              $scope.oldBillingAddress.type     = 'billing';
+              $scope.oldBillingAddress.country  = 'USA';
+
               $scope.updateAddress($scope.oldBillingAddress);
               // $state.go($state.current, {}, {reload: true});
               $scope.billing.address = angular.extend({}, $scope.oldBillingAddress);
+
             } else {
-              $scope.billing.address = angular.extend({}, $scope.shipping.address);
-              $scope.billing.address.label = 'billing';
-              $scope.billing.address.type = 'billing';
+              $scope.billing.address         = angular.extend({}, $scope.shipping.address);
+              $scope.billing.address.label   = 'billing';
+              $scope.billing.address.type    = 'billing';
               $scope.billing.address.country = 'USA';
+
               $scope.createAddress($scope.billing.address);
               $scope.oldBillingAddress = angular.extend({}, $scope.billing.address);
             }
@@ -248,32 +256,36 @@ angular.module('convenienceApp')
           }
         } else {
           // create shipping address
-          $scope.shipping.address.type = 'shipping';
-          $scope.shipping.address.label = 'shipping';
+          $scope.shipping.address.type    = 'shipping';
+          $scope.shipping.address.label   = 'shipping';
           $scope.shipping.address.country = 'USA';
-          $scope.shipping.address.userId = $scope.user._id;
-          $scope.oldShippingAddress = angular.extend({}, $scope.shipping.address);
+          $scope.shipping.address.userId  = $scope.user._id;
+          $scope.oldShippingAddress       = angular.extend({}, $scope.shipping.address);
+
           $scope.createAddress($scope.shipping.address);
 
           if ($scope.sameBillingAsShiping) {
             if (Object.keys($scope.oldBillingAddress).length !== 0) {
+
               $scope.oldBillingAddress.address1 = $scope.shipping.address.address1;
               $scope.oldBillingAddress.address2 = $scope.shipping.address.address2;
-              $scope.oldBillingAddress.city = $scope.shipping.address.city;
-              $scope.oldBillingAddress.state = $scope.shipping.address.state;
-              $scope.oldBillingAddress.zipCode = $scope.shipping.address.zipCode;
-              $scope.oldBillingAddress.userId = $scope.user._id;
-              $scope.oldBillingAddress.label = 'billing';
-              $scope.oldBillingAddress.type = 'billing';
-              $scope.oldBillingAddress.country = 'USA';
+              $scope.oldBillingAddress.city     = $scope.shipping.address.city;
+              $scope.oldBillingAddress.state    = $scope.shipping.address.state;
+              $scope.oldBillingAddress.zipCode  = $scope.shipping.address.zipCode;
+              $scope.oldBillingAddress.userId   = $scope.user._id;
+              $scope.oldBillingAddress.label    = 'billing';
+              $scope.oldBillingAddress.type     = 'billing';
+              $scope.oldBillingAddress.country  = 'USA';
+
               $scope.updateAddress($scope.oldBillingAddress);
               // $state.go($state.current, {}, {reload: true});
               $scope.billing.address = angular.extend({}, $scope.oldBillingAddress);
             } else {
-              $scope.billing.address = angular.extend({}, $scope.shipping.address);
-              $scope.billing.address.label = 'billing';
-              $scope.billing.address.type = 'billing';
+              $scope.billing.address         = angular.extend({}, $scope.shipping.address);
+              $scope.billing.address.label   = 'billing';
+              $scope.billing.address.type    = 'billing';
               $scope.billing.address.country = 'USA';
+
               $scope.createAddress($scope.billing.address);
               $scope.oldBillingAddress = angular.extend({}, $scope.billing.address);
             }
@@ -281,28 +293,31 @@ angular.module('convenienceApp')
             $scope.shippingForm.$setPristine();
           }
         }
+        $scope.shippingForm.$setPristine();
       }
 
-      if ($scope.billingForm.$dirty && $scope.billingForm.$valid &&
+      if ($scope.billingForm.$dirty       && $scope.billingForm.$valid &&
           $scope.billing.address.address1 && $scope.billing.address.address1 !== '' &&
-          $scope.billing.address.city && $scope.billing.address.city !== '' &&
+          $scope.billing.address.city     && $scope.billing.address.city     !== '' &&
           $scope.billing.address.address2 && $scope.billing.address.address2 !== '' &&
-          $scope.billing.address.zipCode && $scope.billing.address.zipCode !== '' &&
-          $scope.billing.address.state && $scope.billing.address.state !== '') {
+          $scope.billing.address.zipCode  && $scope.billing.address.zipCode  !== '' &&
+          $scope.billing.address.state    && $scope.billing.address.state    !== '') {
 
         if (Object.keys($scope.oldBillingAddress).length !== 0) {
           // update billing address
           $scope.billing.address.userId = $scope.user._id;
-          $scope.oldBillingAddress = angular.extend({}, $scope.billing.address);
+          $scope.oldBillingAddress      = angular.extend({}, $scope.billing.address);
+
           $scope.updateAddress($scope.oldBillingAddress);
           $scope.billingForm.$setPristine();
         } else {
           // create billing address
-          $scope.billing.address.type = 'billing';
-          $scope.billing.address.label = 'billing';
+          $scope.billing.address.type    = 'billing';
+          $scope.billing.address.label   = 'billing';
           $scope.billing.address.country = 'USA';
-          $scope.billing.address.userId = $scope.user._id;
-          $scope.oldBillingAddress = angular.extend({}, $scope.billing.address);
+          $scope.billing.address.userId  = $scope.user._id;
+          $scope.oldBillingAddress       = angular.extend({}, $scope.billing.address);
+
           $scope.createAddress($scope.billing.address);
           $scope.billingForm.$setPristine();
         }
