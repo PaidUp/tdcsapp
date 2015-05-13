a907e01bbc7c53fcb0abf6ddb90b7bd3f4d36fee
Jesse Cogollo <cogollo87@gmail.com>
2015-05-13 16:50:53 -0500

    add validate form front end

--- client/app/commerce/provider/request/request.controller.js
+++ client/app/commerce/provider/request/request.controller.js
@@ -1,14 +1,15 @@
 'use strict';
 
 angular.module('convenienceApp')
-  .controller('ProviderRequestCtrl', function ($scope) {
-
-    
-    
-    $scope.registerProvider = function(providerForm){
-        /*console.log('providerForm.dirty0',providerForm.dirty);
-        console.log('providerForm',$scope.providerForm);
-        console.log('providerForm.dirty',$scope.providerForm.dirty);
+  .controller('ProviderRequestCtrl', function ($scope, providerService) {
+    $scope.submitted = false;
+    $scope.registerProvider = function(){
+        $scope.submitted = true;
+        if($scope.providerForm.$valid && $scope.ownerForm.$valid && $scope.billingForm.$valid){
+            //console.log('listo para conectarse con el servicio provider');
+            providerService.saveProvider($scope.team,$scope.owner,$scope.billing);
+        };
+        /*console.log('providerForm.dirty',$scope.providerForm.dirty);
         console.log('dirty',$scope.dirty);
         console.log('team',$scope.team);
         console.log('owner',$scope.owner);
