af9bc06d8618ddc99ae6339a65381a5fcd55ac77
Jesse Cogollo <cogollo87@gmail.com>
2015-05-13 14:57:43 -0500

    send all data neccesary for register provider info to controller

--- client/app/commerce/provider/request/request.controller.js
+++ client/app/commerce/provider/request/request.controller.js
@@ -2,5 +2,18 @@
 
 angular.module('convenienceApp')
   .controller('ProviderRequestCtrl', function ($scope) {
+
+    
+    
+    $scope.registerProvider = function(providerForm){
+        /*console.log('providerForm.dirty0',providerForm.dirty);
+        console.log('providerForm',$scope.providerForm);
+        console.log('providerForm.dirty',$scope.providerForm.dirty);
+        console.log('dirty',$scope.dirty);
+        console.log('team',$scope.team);
+        console.log('owner',$scope.owner);
+        console.log('billing',$scope.billing);*/
+    }
+
     
   });
