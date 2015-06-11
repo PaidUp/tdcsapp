9f0c1cc9e1be474ff05312ebc86604b22cb7942a
Jesse Cogollo <cogollo87@gmail.com>
2015-06-11 17:29:34 -0500

    implement create product mangeto. I

--- server/api/commerce/provider/provider.spec.js
+++ server/api/commerce/provider/provider.spec.js
@@ -75,7 +75,7 @@ describe('User', function() {
 
     describe('provider.controller', function() {
 
-        it.only('save', function (done) {
+        it.only('response', function (done) {
             var provider = new Provider(providerSpecModel.provider);
             providerService.save(provider, function(err, data){
                 assert.equal(err, null);
