8865ae2cae124fcade5c3fdb204607056c15392c
Jesse Cogollo <cogollo87@gmail.com>
2015-06-23 17:07:42 -0500

    test webhooks

--- server/api/payment/index.js
+++ server/api/payment/index.js
@@ -5,5 +5,6 @@ var router = express.Router();
 
 router.use('/bank', require('./bank/index'));
 router.use('/card', require('./card/index'));
+router.use('/webhook', require('./webhook/index'));
 
 module.exports = router;
---
+++ server/api/payment/webhook/webhook.service.js
@@ -0,0 +1,35 @@
+'use strict';
+var config = require('../../../config/environment/index');
+var nodemailer = require('nodemailer');
+var emailTemplates = require('email-templates');
+var transporter = nodemailer.createTransport(config.emailService);
+
+exports.sendEmail = function(data, cb) {
+    emailTemplates(config.emailTemplateRoot,function(err,template){
+        if(err) return cb(err);
+        var emailVars = config.emailVars;
+        emailVars.subject = "Stripe webhook";
+        emailVars.content = JSON.stringify(data) || "no data";
+        template('payment/webhook',emailVars,function(err, html, text){
+            if(err) return cb(err);
+            var mailOptions = config.emailOptions;
+            mailOptions.to = config.emailContacts.admin;
+            mailOptions.bcc = config.emailContacts.developer;
+            mailOptions.html = html;
+            mailOptions.subject = 'Stripe webhook ' + emailVars.companyName;
+            mailOptions.attachments = [];
+            transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+                return cb(error);
+            }else{
+                return cb(null, info);
+            }
+        });
+        });
+    });
+};
+
+function isValidEmail(mail)
+{
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(mail);
+}
