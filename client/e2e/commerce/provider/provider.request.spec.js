d14a599fd8b24a9bfb06f5a4b7c046c539223509
Jesse Cogollo <cogollo87@gmail.com>
2015-07-01 15:21:45 -0500

    happy path provider request, Done.

--- client/e2e/commerce/provider/provider.request.spec.js
+++ client/e2e/commerce/provider/provider.request.spec.js
@@ -1,49 +1,78 @@
 'use strict';
 //var Utils = require('../../utils/utils');
-//var models = require('../../models');
+var modelProvider = require('../../models').provider;
 var commerceRequestpo = require('../../page-objects/commerceRequest.po.js');
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 var expectChai = chai.expect;
-//var driver = browser.driver;
 
 describe('Provider request', function () {
-  var teamName, btnSave, ownerFirstName, ownerLastName, month, day, year;
+  var teamName, btnSave, ownerFirstName, ownerLastName, month, day, year, Address, ownerEmail, ownerPhone, city, state,
+  zipCode, ownerSSN, businessType, businessName, EIN, aba, dda, ddaVerification;
   beforeEach(function() {
-      //browser.get('/commerce/provider/request');
       teamName = element(by.model('provider.teamName'));
       ownerFirstName = element(by.model('provider.ownerFirstName'));
       ownerLastName = element(by.model('provider.ownerLastName'));
       month = element(by.model('provider.date.month'));
       day = element(by.model('provider.date.day'));
       year = element(by.model('provider.date.year'));
+      Address = element(by.model('provider.Address'));
+      ownerEmail = element(by.model('provider.ownerEmail'));
+      ownerPhone = element(by.model('provider.ownerPhone'));
+      city = element(by.model('provider.city'));
+      state = element(by.model('provider.state'));
+      zipCode = element(by.model('provider.zipCode'));
+      ownerSSN = element(by.model('provider.ownerSSN'));
+      businessType = element(by.model('provider.businessType'));
+      businessName = element(by.model('provider.businessName'));
+      EIN = element(by.model('provider.EIN'));
+      aba = element(by.model('provider.aba'));
+      dda = element(by.model('provider.dda'));
+      ddaVerification = element(by.model('provider.ddaVerification'));
       btnSave = element(by.id('btnSave'));
   });
   
-  it("coach can not see options of parent and pre charged info.", function () {
-    var addAthleteBtn = element(by.css('button#add-athlete-btn'));
-    console.log('addAthleteBtn',addAthleteBtn);
-    expect(addAthleteBtn.getAttribute('innerText')).toEqual('');
-
-    expect(ownerFirstName.getAttribute('value')).toEqual('Zuly');
-    expect(ownerLastName.getAttribute('value')).toEqual('Alcaraz');
-    btnSave.click();
+  it("coach can not see options of parent and pre charged info.", function (done) {
+    browser.get('/commerce/provider/request');
+    element.all(by.css('button#add-athlete-btn')).then(function(elements) {
+      expect(elements).toEqual([]);//validate button#add-athlete-btn element not exist.
+      expect(ownerFirstName.getAttribute('value')).toEqual('Zuly');
+      expect(ownerLastName.getAttribute('value')).toEqual('Alcaraz');
+      done();
+    });
   });
 
-  /*it("should fill form provider request", function() {
-    //browser.get('/commerce/provider/request');
-    //teamName.sendKeys('america de cali');
-    element(by.css('form[name=checkoutForm]')).submit();
-    browser.wait(function () {
-      return browser.isElementPresent(element(by.css('.titleThankyouBanner')));
-    }, 15000);
+  it("should fill form provider request", function (done) {
+    btnSave.click();
+    //TODO: validate all message. for now happy path.
+    //var nameRequired = element(by.id('nameRequired'));
+    //expect(nameRequired.isDisplayed()).toBe(true);;
+    teamName.sendKeys(modelProvider.teamName);
+    //expect(nameRequired.isDisplayed()).toBe(false);
+    month.sendKeys(modelProvider.month);
+    day.sendKeys(modelProvider.day);
+    year.sendKeys(modelProvider.year);
+    Address.sendKeys(modelProvider.Address);
+    ownerEmail.sendKeys(modelProvider.ownerEmail);
+    ownerPhone.sendKeys(modelProvider.ownerPhone);
+    city.sendKeys(modelProvider.city);
+    state.sendKeys(modelProvider.state);
+    zipCode.sendKeys(modelProvider.zipCode);
+    ownerSSN.sendKeys(modelProvider.ownerSSN);
+    businessType.sendKeys(modelProvider.businessType);
+    businessName.sendKeys(modelProvider.businessName);
+    EIN.sendKeys(modelProvider.EIN);
+    aba.sendKeys(modelProvider.aba);
+    dda.sendKeys(modelProvider.dda);
+    ddaVerification.sendKeys(modelProvider.ddaVerification);
+    btnSave.click();
     browser.getLocationAbsUrl().then(function (url) {
-      expect(url).equal('/commerce/checkout/success');
-      var thankyouTitle = element(by.css('.titleThankyouBanner')).getText();
-      expect(thankyouTitle).equal('Thanks for your order!');
+      expect(url).toEqual('/commerce/provider/success');
     });
-    
-  });*/
+    /*browser.wait(function () {
+    }, 5000);*/
+    done();
+  });
 
 });
