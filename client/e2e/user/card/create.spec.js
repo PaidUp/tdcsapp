98a7d23ee98a5da327574c7a2de1632cec356a13
Jesse Cogollo <cogollo87@gmail.com>
2015-07-03 11:25:58 -0500

    add credit card

--- client/e2e/user/card/create.spec.js
+++ client/e2e/user/card/create.spec.js
@@ -6,32 +6,12 @@ var expectChai = chai.expect;
 var driver = browser.driver;
 
 describe('User update account', function () {
-  var firstName, lastName, email, password, sphone, saddress1, scity, saddress2, sstate, szipCode, sameBillingAsShiping,
-  baddress1, baddress2, bstate, bzipCode, btnSave;
-  beforeEach(function() {
-      /*firstName = element(by.model('user.firstName'));
-      lastName = element(by.model('user.lastName'));
-      email = element(by.model('user.email'));
-      password = element(by.model('user.password'));
-      sphone = element(by.model('shipping.phone'));
-      saddress1 = element(by.model('shipping.address.address1'));
-      scity = element(by.model('shipping.address.city'));
-      saddress2 = element(by.model('shipping.address.address2'));
-      sstate = element(by.model('shipping.address.state'));
-      szipCode = element(by.model('shipping.address.zipCode'));
-      sameBillingAsShiping = element(by.model('sameBillingAsShiping'));
-      baddress1 = element(by.model('billing.address.address1'));
-      baddress2 = element(by.model('billing.address.address2'));
-      bstate = element(by.model('billing.address.state'));
-      bzipCode = element(by.model('billing.address.zipCode'));
-      btnSave = element(by.id('btnSaveAccount'));*/
-  });
-  
+  var temp;
   it("charge 0 cards", function () {
     browser.get('/user/payments');
     var cards = element.all(by.repeater('card in cards'));
     expect(cards.count()).toEqual(0);
-    element(by.id('hrefAddNewCard')).click();
+    temp = element(by.id('hrefAddNewCard')).click();
     //done();
   });
 
@@ -51,18 +31,12 @@ describe('User update account', function () {
   });
 
   it("credict card created.", function () {
-    //expect(browser.getCurrentUrl()).toBe('/user/payments');
-    return browser.sleep(10000).then(function() {
-      browser.get('/user/payments').then(function(){
-      });
+    //browser.waitForAngular()
+    //expect(temp.isDisplayed());
+    element.all(by.repeater('card in cards')).then(function(elements) {
+      return expect(elements).toEqual([]);//validate button#add-athlete-btn element not exist.
+      //expect(element(by.id('hrefAddNewCard')).getText()).toEqual('ADD NEW CARD');
     });
-    //TODO
-    expect(browser.getCurrentUrl()).toBe('/user/payments');
-    expect(element(by.id('hrefAddNewCard')).isPresent()).toEqual(true);
-    var cards = element.all(by.repeater('card in cards'));
-    expect(cards.count()).toEqual(2);
-    console.log('4');
-
   });
 
 });
