d61ee80687614eb731f50c9684e5344e21016722
Ricardo Lara <riclara@gmail.com>
2015-10-06 16:37:28 -0500

    partial 9 cs-581

--- client/app/athletes/update/update-athlete-modal.controller.js
+++ client/app/athletes/update/update-athlete-modal.controller.js
@@ -24,7 +24,6 @@ angular.module('convenienceApp')
       }else{
         $scope.athleteEdit = {};
       }
-      console.log('$rootScope.athleteEdit',$rootScope.athleteEdit);
     };
 
     $scope.isValidDate = function () {
