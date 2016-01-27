4ee5ce4191aef2ff0337e44044cea75a7e6900ad
Ricardo Lara <riclara@gmail.com>
2016-01-27 10:24:52 -0600

    add tracker for login Facebook

--- client/components/auth/directives/facebook/facebook-btn.controller.js
+++ client/components/auth/directives/facebook/facebook-btn.controller.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('convenienceApp')
-  .controller('FacebookBtnCtrl', function ($scope, $rootScope, FlashService, AuthService, $state, ModalService) {
+  .controller('FacebookBtnCtrl', function ($scope, $rootScope, FlashService, AuthService, $state, ModalService, TrackerService) {
     $scope.modal = ModalService;
     $scope.facebookLogin = function() {
       var success = function(user) {
+        var roleType = AuthService.getIsParent() ? 'user' : 'couch'
+        TrackerService.create('Login Facebook', {roleType : roleType});
         $scope.modal.closeModal();
         $state.go(AuthService.getDest());
         AuthService.setDest();
