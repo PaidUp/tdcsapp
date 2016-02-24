79f0222f945d35dfd81dc241b309655157bc4a4c
Jesse Cogollo <cogollo87@gmail.com>
2016-02-24 15:44:08 -0500

    recovery e2e test 8-% complete

--- client/e2eNW/user/account/update.account.spec.js
+++ client/e2eNW/user/account/update.account.spec.js
@@ -14,17 +14,47 @@ module.exports = {
     login.logout(browser);
   },
 
-  'updateProfile' : function (browser) {
-    //verify fullname before update profile
-    //update first and last name
-    //verify fullname before update profile
+  'preValidateForm': function (browser) {
+    browser.getText(".dropdown-toggle", function(result) {
+      this.assert.equal(result.value, "Zuly Alcaraz");
+    });
+  },
+  'updateProfileAccount' : function (browser) {
     browser
-      //.waitForElementVisible('#btnCreateCard', 1000)
-      //.setValue('#btnCreateCard', browser.Keys.ENTER)
-      //.pause(20000)
-      //.url(function(url){
-        //this.assert.equal(url.value,'http://localhost:9000/user/payments')
-      //})
+      .clearValue('input[name=firstName]')
+      .setValue('input[name=firstName]', 'change name')
+      .clearValue('input[name=lastName]')
+      .setValue('input[name=lastName]', 'change last')
       .pause(1000)
+  },
+  'updatePasswordAccount' : function (browser) {
+    browser
+      .setValue('input[name=password]', 'newPassword')
+      .setValue('input[name=confirm]', 'newPassword')
+  },
+
+  'updateContactInfoAccount' : function (browser) {
+    browser
+      .clearValue('input[name=phone]')
+      .setValue('input[name=phone]', '9877899871')
+      .setValue('input[name=address1]', 'new street')
+      .setValue('input[name=address2]', 'avenue')
+      .setValue('input[name=city]', 'Austin')
+      .setValue('#state', 'TX')
+      .setValue('input[name=zipCode]', '00000')
+      .setValue('#chksameBillingAsShiping', browser.Keys.TAB)
+      .setValue('#chksameBillingAsShiping', browser.Keys.SPACE)
+      .pause(2000)
+  },
+  'submitForm' : function (browser) {
+    browser
+      .setValue('#btnSaveAccount', browser.Keys.ENTER)
+      .pause(4000)
+  },
+  'validateChangesAccount' : function (browser) {
+    browser.assert.value("input[name=firstName]", "change name");
+    browser.getText(".dropdown-toggle", function(result) {
+      this.assert.equal(result.value, "change name change last");
+    });
   }
 };
