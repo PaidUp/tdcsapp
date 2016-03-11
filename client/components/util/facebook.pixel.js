cc8f0296605ca6f7ae8517b20d802e352bade64b
Ricardo Lara <riclara@gmail.com>
2016-03-11 09:55:56 -0600

    updates scripts

--- client/components/util/facebook.pixel.js
+++ client/components/util/facebook.pixel.js
@@ -1,14 +1,12 @@
 /**
  * Created by riclara on 1/18/16.
  */
-<!-- Facebook Pixel Code -->
+
 !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
   n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
   n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
   t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
   document,'script','//connect.facebook.net/en_US/fbevents.js');
 
-fbq('init', '175148602845852');
+fbq('init', '291498354381832');
 fbq('track', "PageView");
-
-<!-- End Facebook Pixel Code -->
